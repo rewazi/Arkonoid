5e3bcd66c6f75f04e9121951122f38f388ab8c0d
rewazi <maksim.koroljov@ivkhk.ee>
2024-12-04 13:20:17 +0200

    Arranging objects on the screen

--- Canvas_game_files/game.js
+++ Canvas_game_files/game.js
@@ -1,12 +1,63 @@
 let game = {
-    start: function(){
-        this.ctx = document.getElementsById("mycanvas").getContext("2d");
-        let background = new Image();
-        window.requestAnimationFrame(() =>{
-            this.ctx.drawImage(background, 0, 0);
+    ctx: null,
+    platform: null,
+    ball: null,
+    sprites:{
+        background: null,
+        ball: null,
+        platform: null
+    },
+    init: function(){
+        this.ctx = document.getElementById("mycanvas").getContext("2d");
+    },
+    preload(callback){
+        let loaded = 0;
+        let required = Object.keys(this.sprites).length;
+
+        for(let key in this.sprites) {
+            this.sprites[key] = new Image();
+            this.sprites[key].src = "img/" + key + ".png";
+            this.sprites[key].addEventListener("load",() =>{
+                ++loaded
+                if(loaded >= required){
+                    callback();
+                }
+            });
+        }
+    },
+    run(){
+        window.requestAnimationFrame(() => {
+            this.render();
         });
+    },
+    render(){
+        this.ctx.drawImage(this.sprites.background, 0, 0)
+        this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
+         this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+    },
+    start: function(){
+        this.init();
+        this.preload(() =>{
+            this.run();
+        });
+
     }
 };
-window.addEventListener("load", () =>{
+
+game.ball ={
+    x: 320,
+    y: 280,
+    width: 20,
+    height: 20
+};
+
+game.platform = {
+    x: 280,
+    y: 300
+};
+
+
+window.addEventListener("load",() =>{
     game.start();
-});
+})
