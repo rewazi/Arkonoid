7e705c1fa306b177f39ff71f3495d67e0a94971c
rewazi <maksim.koroljov@ivkhk.ee>
2024-12-13 11:23:35 +0200

    Victory at the level

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ let game = {
     platform: null,
     ball: null,
     blocks: [],
+    score: 0,
     rows: 4,
     cols: 8,
     width:640,
@@ -63,7 +64,6 @@ let game = {
                     active: true,
                     width:60,
                     height: 20,
-
                     x: 64 * col + 65,
                     y: 24 * row + 35
                 });
@@ -77,13 +77,19 @@ let game = {
         this.platform.collideWorldBounds();
         this.platform.move();
         this.ball.move();
-        
-       
+    },
+    addScore(){
+        ++this.score;
+
+        if (this.score >= this.blocks.length){
+            this.end("вы победили");
+        }
     },
     collideBlocks(){
         for(let block of this.blocks){
             if(block.active && this.ball.collide(block)){
                 this.ball.bumpBlock(block);
+                this.addScore();
             }
         }
     },
@@ -123,6 +129,11 @@ let game = {
             this.run();
         });
 
+    },
+    end(message){
+        this.running = false;
+        alert(message);
+        window.location.reload();
     },
     random(min, max){
         return Math.floor(Math.random()*(max-min+1)+min)
