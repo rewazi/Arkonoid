b49d98bed49c03415627a5670a627f0095d9cb05
rewazi <maksim.koroljov@ivkhk.ee>
2024-12-15 05:57:56 +0200

    ugly version

--- game.js
+++ game.js
@@ -11,8 +11,8 @@ let game = {
     ball: null,
     blocks: [],
     score: 0,
-    rows: 4,
-    cols: 8,
+    rows: 6,    
+    cols: 10,   
     width: 640,
     height: 360,
     sprites: {
@@ -59,6 +59,7 @@ let game = {
             }
         };
 
+      
         this.preloadSprites(onResourceLoad);
         this.preloadAudio(onResourceLoad);
     },
@@ -116,14 +117,14 @@ let game = {
             if (block.active && this.ball.collide(block)) {
                 this.ball.bumpBlock(block);
                 this.addScore();
-                this.sounds.bump.play();
+                this.sounds.bump && this.sounds.bump.play();
             }
         }
     },
     collidePlatform() {
         if (this.ball.collide(this.platform)) {
-            this.ball.bumpPlatform(this.platform)
-            this.sounds.bump.play();
+            this.ball.bumpPlatform(this.platform);
+            this.sounds.bump && this.sounds.bump.play();
         }
     },
     run() {
@@ -136,12 +137,15 @@ let game = {
         }
     },
     render() {
+      
         this.ctx.clearRect(0, 0, this.width, this.height);
-        this.ctx.drawImage(this.sprites.background, 0, 0);
-        // Исправлено: использование frame для выбора кадра анимации
+        this.ctx.fillStyle = "#000080"; 
+        this.ctx.fillRect(0, 0, this.width, this.height);
+
+        
         this.ctx.drawImage(
             this.sprites.ball,
-            this.ball.frame * this.ball.width, // рассчитываем кадр
+            this.ball.frame * this.ball.width,
             0,
             this.ball.width,
             this.ball.height,
@@ -150,14 +154,21 @@ let game = {
             this.ball.width,
             this.ball.height
         );
-        this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+
+       
+        this.ctx.fillStyle = "#00FF00"; 
+        this.ctx.fillRect(this.platform.x, this.platform.y, this.platform.width, this.platform.height);
+
         this.renderBlocks();
+        this.ctx.fillStyle = "#FFFFFF";
         this.ctx.fillText("Score: " + this.score, 15, 20);
     },
     renderBlocks() {
+        
         for (let block of this.blocks) {
             if (block.active) {
-                this.ctx.drawImage(this.sprites.block, block.x, block.y);
+                this.ctx.fillStyle = "#FF0000"; 
+                this.ctx.fillRect(block.x, block.y, block.width, block.height);
             }
         }
     },
@@ -181,12 +192,12 @@ let game = {
 game.ball = {
     dx: 0,
     dy: 0,
-    velocity: 3,
+    velocity: 5, 
     x: 320,
     y: 280,
     width: 20,
     height: 20,
-    frame: 0, // Инициализируем кадр анимации
+    frame: 0,
     start() {
         this.dy = -this.velocity;
         this.dx = game.random(-this.velocity, this.velocity);
@@ -220,7 +231,7 @@ game.ball = {
         ) {
             return true;
         }
-        return false
+        return false;
     },
     collideWorldBounds() {
         let x = this.x + this.dx;
